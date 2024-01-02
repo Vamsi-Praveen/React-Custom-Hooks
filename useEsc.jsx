b3313406beaccc07a7d137e3daf8fec0cf4d1d59
Vamsi Praveen Nakka <98261619+Vamsi-Praveen@users.noreply.github.com>
2024-01-02 21:12:13 +0530

    Update useEsc.jsx

--- useEsc.jsx
+++ useEsc.jsx
@@ -1,9 +1,12 @@
+//This hook is used to achieve the behavior of when a element is toggle by a trigger button when click esc button it closes the menu thus increases the user experience
 import { useEffect } from "react"
 
 const useEsc = (handler) => {
     useEffect(() => {
+        //checking the button pressed is Esc button or not
         const handleEscPress = (e) => {
             if (e.key == 'Escape') {
+                //if the button is Escape then call the handler function the handler functionc contains business logic i.e simply changing the state to false
                 handler();
             }
         }
